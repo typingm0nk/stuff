57111d5a790d82af893c97cbbbdec06c7768fe61
Malte Schmidt <typingm0nk@users.noreply.github.com>
2016-04-23 12:59:00 +0200

    Added files via upload

--- eea.py
+++ eea.py
@@ -1,35 +1,53 @@
 #!/usr/bin/env python
 
-def q(a,b):
-    r=a%b
+def q(aa,bb):
+    a=0
+    b=aa
+    q=0
+    u=0
+    s=1
+    v=1
+    t=0
+    print("a","b","q","u","s","v","t")
+    print(a,b,q,u,s,v,t)
+    a=aa
+    b=bb
     q=a//b
-    while r>1:
+    u=1
+    s=0
+    v=0
+    t=1
+    print(a,b,q,u,s,v,t)
+    while b>0:
+        r=a%b
+        q=a//b
         a=b
         b=r
-        q=a//b
-        r=a%b
-        print(a," = ",q,"*",b," + ",r)
-    if r==1:
-        return q
-    else:
-        False
+        if b==0:
+            return q #TODO
+        uu=s
+        vv=t
+        s=u-q*s
+        t=v-q*t
+        u=uu
+        v=vv
+        print(a,b,q,u,s,v,t)
+    return(u)
 
 def eea(m,e):
     if m==0 or e==0 or e>m: #check if input is correct
         return False
-    x=q(m,e)
-    d=(x*m+1)//e
-    return(d)
+    return(q(m,e))
 
 def check(d,e,m):
-    r = d*e %m
+    r = (d*e) %m
     if r==1:
         return True
     else:
-        return False
+        return ("r:",r)
 
-e=2**16+1
-m=173405
+e=78
+m=99
 d=eea(m,e)
 print(d,e,m)
 print(check(d,e,m))
