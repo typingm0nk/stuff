b675cc5dcd1e7fc45dc527449536e61c31daa196
Malte Schmidt <typingm0nk@users.noreply.github.com>
2016-04-24 17:26:46 +0200

    works now, some checks added

--- eea.py
+++ eea.py
@@ -1,53 +1,48 @@
 #!/usr/bin/env python
+# -*- coding: cp1252 -*-
 
-def q(aa,bb):
-    a=0
-    b=aa
-    q=0
-    u=0
-    s=1
-    v=1
-    t=0
-    print("a","b","q","u","s","v","t")
-    print(a,b,q,u,s,v,t)
-    a=aa
-    b=bb
-    q=a//b
-    u=1
-    s=0
+def d(a,b):
     v=0
     t=1
-    print(a,b,q,u,s,v,t)
     while b>0:
         r=a%b
         q=a//b
         a=b
         b=r
-        if b==0:
-            return q #TODO
-        uu=s
         vv=t
-        s=u-q*s
         t=v-q*t
-        u=uu
         v=vv
-        print(a,b,q,u,s,v,t)
-    return(u)
+    return(v)
 
 def eea(m,e):
     if m==0 or e==0 or e>m: #check if input is correct
         return False
-    return(q(m,e))
+    return(d(m,e))
 
 def check(d,e,m):
     r = (d*e) %m
     if r==1:
         return True
     else:
-        return ("r:",r)
+        return ("ups, something went wrong.",d,e,m)
 
-e=78
-m=99
-d=eea(m,e)
-print(d,e,m)
-print(check(d,e,m))
+def teilerfremd(a,b):
+    r=2
+    while r>1:
+        r=a%b
+        if r==1:
+            return True
+        a=b
+        b=r
+    return False
+
+m=12345678901234567890
+e=2**16+1
+if teilerfremd(e,m):
+    d=eea(m,e)
+    while d<0: #assuming we prefer a positiv d
+        d+=m
+    print(d,e,m)
+    print(check(d,e,m))
+else:
+    print("e und m müssen teilerfremd sein")
